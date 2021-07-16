a5fda4a218ab7e04c838fe2467008ea7f58ec409
Maria Rebelo <dicarllim@gmail.com>
2021-07-16 11:40:20 -0300

    feat: reading compiled pdf of LGPD

--- lib/pages/lgpd.dart
+++ lib/pages/lgpd.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class LGPD extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1A3F6D),
+        title: Text('LGPD'),
+      ),
+       body: SfPdfViewer.asset(
+        'assets/L13709compilado.pdf',
+      ),
     );
   }
 }
